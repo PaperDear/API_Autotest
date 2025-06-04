3865a194cebb091e86c356c50df3e75d19d84a84
wjhzt <605857280@qq.com>
2025-06-04 19:18:11 +0800

    1.参数提取并渲染 2.数据库+json断言

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import pytest
 import os
 if __name__ == '__main__':
-    pytest.main(['-vs', './case/test_case.py',
+    pytest.main(['-vs', './testcases/test_case.py',
                  '--alluredir=./report/json_report', '--clean-alluredir'])
     os.system(r"allure generate ./report/json_report -o ./report/html_report --clean")
--- utils/excel_utils.py
+++ utils/excel_utils.py
@@ -1,18 +1,29 @@
 import openpyxl
 import os
 
+
 def read_excel():
+    # 测试本文件执行时打开
+    # wb = openpyxl.load_workbook('../data/cases.xlsx')
+
     # 打开excel文件
-    wb=openpyxl.load_workbook(os.getcwd()+'/data/data.xlsx')
-    # 选择表
-    sheet=wb["Sheet1"]
+    wb = openpyxl.load_workbook(os.getcwd() + '/data/cases.xlsx')
+    # 选择表sheet1
+    sheet = wb["Sheet1"]
     # 读数据
-    data=[]
-    keys=[cell.value for cell in sheet[1]]
-    for row in sheet.iter_rows(min_row=2,values_only=True):
-        dict_data=dict(zip(keys, row))
-        data.append(dict_data)
+    data = []
+    # 首行为中文注解，提取key从第二行的英文开始
+    keys = [cell.value for cell in sheet[2]]
+    # value从第3行开始提取
+    for row in sheet.iter_rows(min_row=3, values_only=True):
+        dict_data = dict(zip(keys, row))
+        # 判断是否是需要执行的用例
+        if dict_data['isTrue']:
+            data.append(dict_data)
     print(data)
     # 关闭文件
     wb.close()
     return data
+
+# if __name__ == '__main__':
+#     read_excel()
